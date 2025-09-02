7f65ca9ad1ab0719bcbc0769a772df5bff79ca0d
jerrieperrie <jeanpierredutoit1@gmail.com>
2025-09-02 10:17:24 +1000

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+funtion Footer(){
+  return(
+    <footer style = {}>
+      <p></p>
+      <div style = {}>
+        <a href = "https://bookstore.com" style = {footerStyle}> Bookstore </a>
+        <a href = "https://facebook.com/bookstore" style = {footerStyle}> Facebook </a>
+        <a href = "https://instagram.com/bookstore" style = {footerStyle}> Instagram </a>
+        <a href = "https://twitter.com/bookstore" style = {footerStyle}> Twitter </a>
+      </div>
+    </footer>
+  );
+}
+
+const footerStyle = {
+  ?
+};
+
+export default Footer;
