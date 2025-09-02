eae6a8b50aaa98efc49441ab40286cdf85b57cf6
jerrieperrie <jeanpierredutoit1@gmail.com>
2025-09-02 17:27:48 +1000

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import 
 
-  function Header(){
+ const Header = () =>{
     return(
-      <input type = "text" id = "searhInput" placeholder = "Search books, authors and genres">
-      
-      <nav style = {}>
+      <div class = "search-container">
+        <input type = "text" id = "searhInput" placeholder = "Search books, authors and genres">
+        <button id = "searchButton"> Search </button>
+      </div>
+    );
+  <button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>
+  }
+
+export default Header;
+
+
+
+<nav style = {}>
         <Link to = "/" style = {}> Home </Link>
         <Link to = "/browse" style = {}> Browse </Link>
         <Link to = "/cart" style = {}> Cart </Link>
         <Link to = "/account" style = {}> Account </Link>
       </nav>
-    );
-  }
-
-export default Header;
