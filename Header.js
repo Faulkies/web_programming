273f0b0fbf062de947993d670555c7f3cb99019c
jerrieperrie <jeanpierredutoit1@gmail.com>
2025-09-02 10:06:55 +1000

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 
+
+  function Header(){
+    return(
+      <nav style = {}>
+        <Link to = "/" style = {}> Home </Link>
+        <Link to = "/browse" style = {}> Browse </Link>
+        <Link to = "/cart" style = {}> Cart </Link>
+        <Link to = "/account" style = {}> Account </Link>
+      </nav>
+    );
+  }
+
+export default Header;
