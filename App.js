6f57c7e8558d8de7725692b8bcca0276cf33ef9c
jerrieperrie <jeanpierredutoit1@gmail.com>
2025-09-02 10:00:32 +1000

    Update App.js
    Added routing and nav code as well as importing all pages for App.js file

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 import './App.css';
-import Home from './Home';
+import Header from './?/Header';
+import Footer from './?/Footer';
+import Home from './pages/Home';
+import Browse from './pages/Browse';
+import Cart from './pages/Cart';  
+import Account from './pages/Account';
+
+
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Header />
+    
+    //content based on route (check with this one, not sure if right)
+      <main style={}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </Router>
   );
 }
+
+
+//styling (can be made global)
+const linkStyles = {
+  
+};
 
 export default App;
   
