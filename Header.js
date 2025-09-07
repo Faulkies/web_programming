ce00b29e61576bcb2a3bd11d4d66e75bbb590c69
jerrieperrie <jeanpierredutoit1@gmail.com>
2025-09-07 21:22:25 +1000

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import {InputBase, IconButton, Typography} from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   //used for user input
@@ -12,12 +11,11 @@ const Header = () => {
  //take input from user and sets the value of query to the user input
           <InputBase
             placeholder="Search books, authors and genres" //placeholder text
-            inputProps={{ 'aria-label': 'search' }}
+            inputProps={{'aria-label':'search'}}
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            sx={{ ml: 1, flex: 1 }} //spacing
+            sx={{ml:1,flex: 1}} //spacing
           />
-      <SearchIcon />
   );
 };
 
